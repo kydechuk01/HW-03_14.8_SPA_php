b9031d381476d3f3603da8f54c01a320d1b7b1f0
Alexandr Klimok <kydechuk01@gmail.com>
2024-04-22 17:39:00 +0400

    links fix

--- php/footer.php
+++ php/footer.php
@@ -5,10 +5,10 @@
 </main>
   
 <footer>
-Страница проекта на github: <a href="https://github.com/kydechuk01/">kydechuk01/</a><br> 
+Страница проекта на github: <a href="https://github.com/kydechuk01/HW-03_14.8_SPA_php">https://github.com/kydechuk01/HW-03_14.8_SPA_php/</a><br> 
 Выполнил: Александр Климок (<a href="https://github.com/kydechuk01/">kydechuk01</a>) Курс: PHPPRO_19<br>
 Дата: 22.04.2024 (c) 2024 kydechuk01 для skillfactory.ru<br>
-<i>Пароли: admin - 123, Maria - 456, Elena - 789</i>
+<i>Пароли: admin - 123, Maria - 456, Elena - 789</i><br>
 </footer>
 </body>
 </html>
